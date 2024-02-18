ab13cd26f53bd8310551f44bf623bf6aea608320
Alogou Raphael <raphalogou@gmail.com>
2024-02-18 15:08:21 +0100

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+
+export function App() {
+
+  return (
+    <>
+      <main className="min-h-screen flex items-center justify-center">
+        <h1 className="font-bold text-4xl">Welcome to Personal</h1>
+      </main>
+    </>
+  )
+}
